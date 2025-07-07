fff2efbb34038de7a2491693af9a6dd5ef4130da
Mugdha-Naik <mugdhanaik63@gmail.com>
2025-07-08 00:55:53 +0530

    img added in the background

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import pizzaBlur from './photo/pizzaBlur.png'
 
 function App() {
 
@@ -35,27 +36,32 @@ function App() {
   }, [input])
   return (
     <>
-    <div className='font-sans text-center'>
-      <h1>AutoComplete Search Bar</h1>
-      <div>
-    
-    <input type="text"
-    className='p-[5px] w-[500px] border border-gray-600 rounded-2xl'
-    value={input}
-    placeholder='Enter any search'
-    onChange={(e) => setInput(e.target.value)} 
-    onFocus={() => setShowResult(true)}
-    onBlur={() => setShowResult(false)}/>
-    {showResult && (
-      <div className='w-[500px] m-auto overflow-y-scroll max-h-[454px] flex flex-col border-gray-600 rounded-2xl border-[2px] p-[5px] text-left '>
-        {results.map((r)=>(
-          <span className='cursor-pointer hover:bg-gray-100 display-block p-[4px]' key={r.id}>{r.name}</span>
-        ))}
+    <div className="relative w-fit m-auto">
+      <img src={pizzaBlur} alt="pizza" />
+      <div className="absolute top-0 left-1/2 -translate-x-1/2 w-full flex flex-col items-center mt-8">
+        <div className='font-sans text-center bg-white/80 rounded-2xl p-4 shadow-lg'>
+          <h1>AutoComplete Search Bar</h1>
+          <div>
+            <input
+              type="text"
+              className='p-[5px] w-[500px] border border-gray-600 rounded-2xl'
+              value={input}
+              placeholder='Enter any search'
+              onChange={(e) => setInput(e.target.value)}
+              onFocus={() => setShowResult(true)}
+              onBlur={() => setShowResult(false)}
+            />
+            {showResult && (
+              <div className='w-[500px] m-auto overflow-y-scroll max-h-[454px] flex flex-col border-gray-600 rounded-2xl border-[2px] p-[5px] text-left '>
+                {results.map((r) => (
+                  <span className='cursor-pointer hover:bg-gray-100 display-block p-[4px]' key={r.id}>{r.name}</span>
+                ))}
+              </div>
+            )}
+          </div>
+        </div>
       </div>
-    )}
     </div>
-    </div>
-
     </>
   )
 }
